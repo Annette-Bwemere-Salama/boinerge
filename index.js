95300710f98fda626bf9d437e3f9d2e62276a7e4
Annette Bwemere <bwemereannette@gmail.com>
2023-02-15 09:25:12 +0200

    feat: firstRouteé

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
-// import cors from "cors"
-// import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import  {goalRoutes} from "./routes/goalRoutes";
 
 dotenv.config();
 
 const app = express()
 
-const port = process.env.PORT
-// app.use(cors())
+const port = process.env.PORT || 1200
+
 app.use(express.json())
-    
-const server = app.listen(port, ()=>{
+
+app.post('/api/goals', goalRoutes);
+
+app.listen(port, ()=>{
     console.log(`server is running at ${port}`);
 })
---
+++ routes/goalRoutes.js
@@ -0,0 +1,13 @@
+// import express from "express";
+import router from "express";
+router.Router()
+
+export const goalRoutes = async (req, res)=>{
+
+    // router.get('/', (req,res) =>{
+        res.status(200).json({message: 'GetGoal'})
+    // });
+}
+
+
+// module.exports= router; 
