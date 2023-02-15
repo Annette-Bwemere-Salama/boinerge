dd44518e98f2505a1ac37afd1957165bc6817131
Annette Bwemere <bwemereannette@gmail.com>
2023-02-15 11:14:37 +0200

    feat: deleteGoal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
-import  {getGoals, postGoals} from "./routes/goalRoutes";
+import  {getGoals, postGoals, putGoals, deleteGoals} from "./routes/goalRoutes";
 
 dotenv.config();
 
@@ -14,6 +14,8 @@ app.use(express.json())
 
 app.get('/api/goals', getGoals);
 app.post('/api/goals', postGoals);
+app.put('/api/goals/:id', putGoals);
+app.delete('/api/goals/:id', deleteGoals)
 
 
 app.listen(port, ()=>{
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -9,21 +9,22 @@ export const getGoals = async (req,res)=>{
 
 
 export const postGoals = async (req,res)=>{
-    res.status(200).json({message: 'POST Goals'})
+    res.status(200).json({message: 'Set Goals'})
     console.log('GetGoals')
 }
 
 
-// export const getGoals = async (req,res)=>{
-//     res.status(200).json({message: 'Get Goals'})
-//     console.log('GetGoals')
-// }
+export const putGoals = async (req,res)=>{
+    res.status(200).json({message: `Update goal1 ${req.params.id}`})
+    console.log('GetGoals')
+}
 
 
-// export const getGoals = async (req,res)=>{
-//     res.status(200).json({message: 'Get Goals'})
-//     console.log('GetGoals')
-// }
+export const deleteGoals = async (req,res)=>{
+    // res.status(200).json({message: 'Delete Goals'})
+    res.status(200).json({message: `delete goal1 ${req.params.id}`})
+    console.log('GetGoals')
+}
 
 
 
