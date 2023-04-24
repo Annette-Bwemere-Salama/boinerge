97d339995ac85b50316ead19992d265a535465c1
Annette Bwemere <bwemereannette@gmail.com>
2023-04-24 14:25:40 +0200

    add router login and register

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ server.get('/', (req, res) => {
     return res.send('{"name": "Hero", "isLearning": true, "level": "apprentice"}');
 })
 
+server.get("/login", (req, res, next) => {
+    return res.send("Login Annette")
+})
+
+server.get("/register", (req, res, next) => {
+    return res.send("register Annette")
+})
+
 
 server.use((req, res, next) => {
     console.log(`Received ${req.method} request!`);
     next()
 })
+
 server.listen(port, () => {
     console.log(`server is running at ${port}`);
 })
