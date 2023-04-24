c80f99c11a70434e7ad2d205fd2f10555be00817
Annette Bwemere <bwemereannette@gmail.com>
2023-04-24 14:13:19 +0200

    remove all router

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const port = process.env.PORT
 
 
 server.get('/', (req, res) => {
-    return res.send("Hello, Annette")
+    
+    return res.send('{"name": "Hero", "isLearning": true, "level": "apprentice"}');
 })
 
+
+server.use((req, res, next) => {
+    console.log(`Received ${req.method} request!`);
+    next()
+})
 server.listen(port, () => {
     console.log(`server is running at ${port}`);
 })
--- routes/getRouter.js
+++
@@ -1,8 +0,0 @@
-import { getGoals , postGoals, upadateGoals, deleteGoals} from "../controler/getControllers";
-import {Router} from "express";
-const router = Router()
-
-
-export const GoalgetPost = router.get('/', getGoals).post('/', postGoals);
-export const goalUpdateAntDeleteId = router.put('/:id', upadateGoals).delete('/:id', deleteGoals);
-
